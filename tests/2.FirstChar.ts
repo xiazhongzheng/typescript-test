82d31f0a7544eca6028dd898ee9f0c86ba199a0f
xiazhongzheng <1107041574@qq.com>
2021-11-30 10:54:30 +0800

    2.FirstChar

---
+++ tests/2.FirstChar.ts
@@ -0,0 +1,5 @@
+// 获取字符串字面量中的第一个字符
+type FirstChar<T> = T extends `${infer L}${infer R}` ? L : never
+type A = FirstChar<'BFE'> // 'B'
+type B = FirstChar<'dev'> // 'd'
+type C = FirstChar<''> // never
